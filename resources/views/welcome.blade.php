7855d38236a687817f9d0058bc9ab9dffa4a4964
Leandro Melo <statuss@hotmail.com>
2016-12-01 14:04:11 -0300

    Testando Deploy automático

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,6 +82,10 @@
                 </div>
 
                 <div class="links">
+                    <a href="#">Testando Deploy Automático</a>
+                </div>
+
+                <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
